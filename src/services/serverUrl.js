33cb6cf13de37d2b0e44793ddc6c9770d4f8e217
Ajith K. S. <ajithks98086@gmail.com>
2024-08-26 14:14:23 +0530

    Add serverUrl and vercel.json

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,2 +1,5 @@
 // Base URL of the backend server
-export const serverUrl = 'http://localhost:4000'
+// export const serverUrl = 'http://localhost:4000'
+
+export const serverUrl = 'https://project-fair-backend-0q5r.onrender.com'
+
